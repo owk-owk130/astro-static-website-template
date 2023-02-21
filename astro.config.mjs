77cb2578381e4eafc840df40ced5573fefe78f33
owk-owk130 <kengo.owaki@zozo.com>
2023-02-22 00:40:27 +0900

    build settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,16 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    build: {
+      minify: false,
+      rollupOptions: {
+        output: {
+          assetFileNames: '[ext]/[name][extname]',
+          entryFileNames: 'js/[name].js'
+        }
+      }
+    }
+  }
+});
