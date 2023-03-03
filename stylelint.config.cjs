b123c30e374c33f623738ae9733e1146207d6808
owk-owk130 <kengo.owaki@zozo.com>
2023-03-03 11:46:38 +0900

    add stylint

---
+++ stylelint.config.cjs
@@ -0,0 +1,5 @@
+module.exports = {
+  extends: ['stylelint-config-standard-scss', 'stylelint-config-recess-order'],
+  customSyntax: 'postcss-scss',
+  ignoreFiles: ['**/node_modules/**', 'dist/**']
+};
